842616a12d820fd6e692e684c6a87c7659f2f5f5
muller10000 <mullermatias100@hotmail.com>
2024-03-05 00:57:55 -0300

    limite de decision

--- Perceptron.py
+++ Perceptron.py
@@ -10,12 +10,19 @@ En ese concepto de "separación óptima" es donde reside la característica fund
 la máxima distancia (margen) con los puntos que estén más cerca de él mismo. Por eso también a veces se les conoce a las SVM como clasificadores 
 de margen máximo. De esta forma, los puntos del vector que son etiquetados con una categoría estarán a un lado del hiperplano 
 y los casos que se encuentren en la otra categoría estarán al otro lado.
+
 """
 #lectura conjunto de datos
 from sklearn.datasets import load_iris
+#libreria para importar la neurona
+from sklearn.linear_model import Perceptron
+#libreria para data frames
 import pandas as pd
+#libreria para arrays
 import numpy as np
+#libreria para graficos
 import matplotlib.pyplot as plt
+#libreria para 3d
 from mpl_toolkits import mplot3d
 
 #cargamos el conjunto de datos
@@ -29,11 +36,13 @@ print(iris_dataset.target_names)
 print(iris_dataset.data)
 
 # Leemos el conjunto de datos con la libreria Pandas
-
 df = pd.DataFrame(np.c_[iris_dataset['data'], iris_dataset['target']], 
                   columns= iris_dataset['feature_names'] + ['target'])
 print(df)
 
+#representaciones graficas de los datos
+
+"""
 # Representacion grafica de dos dimensiones del conjunto de datos
 
 fig = plt.figure(figsize=(10, 7))
@@ -67,7 +76,6 @@ ax.set_xlabel("petal_length")
 ax.set_ylabel("petal_width")
 ax.set_zlabel("sepal_width")
 
-
 plt.show()
 
 # Representacion grafica de tres dimensiones del conjunto de datos
@@ -93,6 +101,86 @@ ax.set_ylabel("petal_width")
 ax.set_zlabel("sepal_width")
 
 plt.show()
+
+"""
+
+# Reducimos el conjunto de datos para entrenar el algoritmo y visualizar el resultado. nos quedamos solo con el ancho y el largo del petalo.
+df_reduced = df[["petal length (cm)", "petal width (cm)", "target"]]
+# nos quedamos solo con las primeros 2 tipos de flor, setosa y versicolor
+df_reduced = df_reduced.loc[df_reduced["target"].isin([0, 1])]
+print(df_reduced)
+
+# Separamos las etiquetas de salida del resto de caracteristicas del conjunto de datos. Separamos las columnas de datos con la de target
+X_df = df_reduced[["petal length (cm)", "petal width (cm)"]]
+y_df = df_reduced["target"]
+# el target solo queda con la clase 0 y 1 
+print(y_df)
+
+#se crea el perceptron y se lo entrena para ajustar parametros. con libreria sklearn
+clf = Perceptron(max_iter=1000, random_state=40)
+clf.fit(X_df, y_df)
+
+# z(x) = x1*w1 + x2*w2 + b // se busca la funcion matematica que mejor separe los ejemplos de una clase y la otra
+
+# x1*0.9 + x2*1.3 + (-3) // funcion optima. devuelve un valor continuo
+
+# Parametros del modelo (ajusta el peso asociado a cada variable: w1 y w2)
+print(clf.coef_)
+# Terminio de interceptacion (ajusta el termino bias: b)
+print(clf.intercept_)
+
+# REPRESENTACION GRAFICA DEL LIMITE DE DECISION 
+
+# me quedo con los valores de mi conjunto de datos de entrada
+X = X_df.values
+
+# agarro el valor minimo y maximo
+mins= X.min(axis=0) - 0.1
+maxs= X.max(axis=0) + 0.1 
+
+# calculo valores intermedios entre el minimo y el maximo, concretamente 1000 valores
+xx, yy = np.meshgrid(np.linspace(mins[0], maxs[0], 1000),
+                     np.linspace(mins[1], maxs[1], 1000))
+
+# metodo para predecir. en base a los 1000 valores que genere antes
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+fig = plt.figure(figsize=(10, 7))
+
+# Represento la funcion matematica, "limite de decision"
+plt.contourf(xx, yy, Z, cmap="Set3")
+plt.contour(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]), linewidths=1, colors='k')
+
+# agrego sobre el anterior grafico los ejemplos del conjunto de datos, para comprobar si los has separado correctamente
+plt.plot(X[:, 0][y_df==0], X[:, 1][y_df==0], 'bs', label="setosa")
+plt.plot(X[:, 0][y_df==1], X[:, 1][y_df==1], 'go', label="vesicolor")
+
+plt.xlabel("petal_length", fontsize=14)
+plt.ylabel("petal_width", fontsize=14)
+plt.legend(loc="lower right", fontsize=14)
+
+plt.show()
+
+# el limite negro que traza en la figura el PERCEPTRON, es un limite lineal, que dada una coordenada X e Y puede analizar el grafico y comprobar a que especie pertenece la flor
+# a ese limite se le llama "limite de decision"
+
+# entreno la neurona devuelta
+clf = Perceptron(max_iter=1000, random_state=40)
+clf.fit(X_df, y_df)
+
+# esta vez sin pasarle el Y_df (etiqueta), y que almacene la prediccion en y_pred
+y_pred = clf.predict(X_df)
+
+from sklearn.metrics import accuracy_score
+
+# comparo los resultados
+print(accuracy_score(y_df, y_pred))
+
+# como se puede ver en el grafico, ha separado las 2 clases con un 100% de precision
+
+
+
 
 
 
